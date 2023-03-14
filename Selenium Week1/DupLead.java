58e87412c0703120b5065d2b37d16a632ac51acf
kumaranhk <126432237+kumaranhk@users.noreply.github.com>
2023-03-14 22:15:05 +0530

    Add files via upload

---
+++ Selenium Week1/DupLead.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DupLead {
+    public static void main(String[] args) {
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("http://leaftaps.com/opentaps/control/login");
+        driver.manage().window().maximize();
+        driver.findElement(By.name("USERNAME")).sendKeys("DemosalesManager");
+        driver.findElement(By.id("password")).sendKeys("crmsfa");
+        driver.findElement(By.className("decorativeSubmit")).click();
+        driver.findElement(By.linkText("CRM/SFA")).click();
+        driver.findElement(By.linkText("Leads")).click();
+        driver.findElement(By.linkText("Create Lead")).click();
+        driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+        driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Kumaran");
+        driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Arunagiri");
+        driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Hari");
+        driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Mechanical");
+        driver.findElement(By.id("createLeadForm_description")).sendKeys("SWAG");
+        driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("hariharakumaran007@gmail.com");
+
+        WebElement stateDropDown = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+        Select selectStateDropDown = new Select(stateDropDown);
+        selectStateDropDown.selectByVisibleText("New York");
+
+        driver.findElement(By.className("smallSubmit")).click();
+        System.out.println(driver.getTitle());
+        driver.findElement(By.linkText("Duplicate Lead")).click();
+        driver.findElement(By.id("createLeadForm_companyName")).clear();
+        driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Kums Tech solutions");
+        driver.findElement(By.id("createLeadForm_firstName")).clear();
+        driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Maverick");
+        driver.findElement(By.className("smallSubmit")).click();
+
+
+        String expectedResult = "View Lead | opentaps CRM";
+        String actualResult = driver.getTitle();
+        if (actualResult.equals(expectedResult)){
+            System.out.println("Passed");
+        }
+        else {
+            System.out.println("Failed");
+        }
+    }
+}
---
+++ Selenium Week1/EditLead.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class EditLead {
+    public static void main(String[] args) {
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("http://leaftaps.com/opentaps/control/login");
+        driver.manage().window().maximize();
+        driver.findElement(By.name("USERNAME")).sendKeys("DemosalesManager");
+        driver.findElement(By.id("password")).sendKeys("crmsfa");
+        driver.findElement(By.className("decorativeSubmit")).click();
+        driver.findElement(By.linkText("CRM/SFA")).click();
+        driver.findElement(By.linkText("Leads")).click();
+        driver.findElement(By.linkText("Create Lead")).click();
+        driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+        driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Kumaran");
+        driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Arunagiri");
+        driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Hari");
+        driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Mechanical");
+        driver.findElement(By.id("createLeadForm_description")).sendKeys("SWAG");
+        driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("hariharakumaran007@gmail.com");
+
+        WebElement stateDropDown = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+        Select selectStateDropDown = new Select(stateDropDown);
+        selectStateDropDown.selectByVisibleText("New York");
+
+        driver.findElement(By.className("smallSubmit")).click();
+        driver.findElement(By.linkText("Edit")).click();
+        driver.findElement(By.id("updateLeadForm_description")).clear();
+        driver.findElement(By.id("updateLeadForm_importantNote")).sendKeys("MAVERICK");
+        driver.findElement(By.className("smallSubmit")).click();
+
+        String expectedResult = "View Lead | opentaps CRM";
+        String actualResult = driver.getTitle();
+        if (actualResult.equals(expectedResult)){
+            System.out.println("Passed");
+        }
+        else {
+            System.out.println("Failed");
+        }
+
+    }
+}
